b2f59741b7d9f5cb8979d0b3379cab7301441f2b
AutAutmn <103841529+AutAutmn@users.noreply.github.com>
2022-04-17 15:17:03 +0800

    pull commit

--- src/main/java/com/zzs/git/GitTest.java
+++ src/main/java/com/zzs/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello hot fix");
         System.out.println("push repository");
+        System.out.println("pull commit");
     }
 }
